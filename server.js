e531fb270c9d1ba7ee89600041a08152f6121982
Jose <josemurillo1620@gmail.com>
2025-11-03 17:27:02 -0600

    fix: importar cors y escuchar en PORT

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // Cargar variables de entorno
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
